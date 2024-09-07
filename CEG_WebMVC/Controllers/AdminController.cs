9530106626307899cd6b1f6424a39b9e41624b17
Echo2craft <justechojepar@gmail>
2024-09-07 19:16:46 +0700

    Set up Admin Profile Page View Model

--- CEG_WebMVC/Controllers/AdminController.cs
+++ CEG_WebMVC/Controllers/AdminController.cs
@@ -1,4 +1,7 @@
-﻿using CEG_WebMVC.Library;
+﻿using CEG_BAL.ViewModels;
+using CEG_WebMVC.Library;
+using CEG_WebMVC.Models.ViewModels.Admin;
+using Microsoft.AspNetCore.Http.Json;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http.Headers;
 using System.Text.Encodings.Web;
@@ -13,7 +16,7 @@ namespace CEG_WebMVC.Controllers
         private readonly IConfiguration _config;
         private readonly HttpClient _httpClient = null;
         private string AdminAPI_URL = "";
-        private readonly JsonSerializerOptions options = new JsonSerializerOptions
+        private readonly JsonSerializerOptions jsonOptions = new JsonSerializerOptions
         {
             Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
             PropertyNameCaseInsensitive = true
@@ -57,9 +60,55 @@ namespace CEG_WebMVC.Controllers
         [HttpGet("Profile")]
         public async Task<IActionResult> AdminProfile()
         {
+            AdminAPI_URL += "Admin/Profile";
+
             /*if (methcall.GetUrlStringIfUserSessionDataInValid(this, Constants.ADMIN) != null)
                 return Redirect(methcall.GetUrlStringIfUserSessionDataInValid(this, Constants.ADMIN));*/
-            return View();
+
+            string? accToken = HttpContext.Session.GetString(Constants.ACC_TOKEN);
+            string? usrId = HttpContext.Session.GetString(Constants.USR_ID);
+            string? imagePath = HttpContext.Session.GetString(Constants.USR_IMAGE);
+
+            var adminDataAndErrors = new AdminProfileVM();
+            /*var memberInvalidDetails = methcall.GetValidationTempData<AccountViewModel>(this, TempData, Constants.UPDATE_ADMIN_DETAILS_VALID, "adminDetail", jsonOptions);
+            if (memberInvalidDetails != null)
+            {
+                memberInvalidDetails.ImagePath = imagePath;
+                memberInvalidDetails.DefaultUserGenderSelectList = methcall.GetUserGenderSelectableList(memberInvalidDetails.Gender != null ? memberInvalidDetails.Gender : Constants.MALE);
+                adminInvalids.adminDetail = memberInvalidDetails;
+                return View(adminInvalids);
+            }*/
+
+            /*var adminDetails = await methcall.CallMethodReturnObject<GetMemberProfileResponse>(
+                _httpClient: _httpClient,
+                options: jsonOptions,
+                methodName: Constants.POST_METHOD,
+                url: AdminAPI_URL,
+                _logger: _logger,
+                inputType: usrId,
+                accessToken: accToken);
+            if (adminDetails == null || adminDetails.Data == null)
+            {
+                TempData[Constants.ALERT_DEFAULT_ERROR_NAME] =
+                    "Error while processing your request! (Getting Admin Profile!).\n Admin Details Not Found!";
+                return RedirectToAction("AdminIndex");
+            }
+            else
+            if (!adminDetails.Status)
+            {
+                TempData[Constants.ALERT_DEFAULT_ERROR_NAME] =
+                    "Error while processing your request! (Getting Admin Profile!).\n Admin Details Not Found!"
+                + adminDetails.ErrorMessage;
+                return RedirectToAction("AdminIndex");
+            }
+            var adminInvalidPasswordUpdate = methcall.GetValidationTempData<UpdateMemberPassword>(this, TempData, Constants.UPDATE_ADMIN_PASSWORD_VALID, "adminPassword", jsonOptions);
+            if (adminInvalidPasswordUpdate != null)
+            {
+                adminInvalids.adminPassword = adminInvalidPasswordUpdate;
+            }
+            adminDetails.Data.DefaultUserGenderSelectList = methcall.GetUserGenderSelectableList(adminDetails.Data.Gender);
+            adminInvalids.adminDetail = adminDetails.Data;*/
+            return View(adminDataAndErrors);
         }
     }
 }
---
+++ CEG_WebMVC/Models/ViewModels/Admin/AdminProfileVM.cs
@@ -0,0 +1,15 @@
+﻿namespace CEG_WebMVC.Models.ViewModels.Admin
+{
+    public class AdminProfileVM
+    {
+        public AdminProfileVM()
+        {
+            adminPassword = new CEG_BAL.ViewModels.Account.UpdateAccountPassword();
+            adminDetail = new CEG_BAL.ViewModels.AccountViewModel();
+        }
+
+        public CEG_BAL.ViewModels.Account.UpdateAccountPassword adminPassword { get; set; }
+
+        public CEG_BAL.ViewModels.AccountViewModel adminDetail { get; set; }
+    }
+}
