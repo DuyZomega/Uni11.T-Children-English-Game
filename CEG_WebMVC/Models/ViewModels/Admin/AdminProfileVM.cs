2c2ba18a914121cef58db2979c0be130aaccf0fc
Echo2craft <justechojepar@gmail>
2024-09-07 19:50:56 +0700

    Fix Gender drop down list not working

--- CEG_WebMVC/Models/ViewModels/Admin/AdminProfileVM.cs
+++ CEG_WebMVC/Models/ViewModels/Admin/AdminProfileVM.cs
@@ -1,4 +1,6 @@
-﻿namespace CEG_WebMVC.Models.ViewModels.Admin
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CEG_WebMVC.Models.ViewModels.Admin
 {
     public class AdminProfileVM
     {
@@ -6,10 +8,17 @@
         {
             adminPassword = new CEG_BAL.ViewModels.Account.UpdateAccountPassword();
             adminDetail = new CEG_BAL.ViewModels.AccountViewModel();
+            DefaultUserGenderSelectList = new List<SelectListItem>() {
+                new SelectListItem() { Text = "Gender", Value = ""},
+                new SelectListItem { Text = "Male", Value = "Male" },
+                new SelectListItem { Text = "Female", Value = "Female" },
+                new SelectListItem { Text = "Other", Value = "Other" },
+            };
         }
 
         public CEG_BAL.ViewModels.Account.UpdateAccountPassword adminPassword { get; set; }
 
         public CEG_BAL.ViewModels.AccountViewModel adminDetail { get; set; }
+        public List<SelectListItem> DefaultUserGenderSelectList { get; set; }
     }
 }
