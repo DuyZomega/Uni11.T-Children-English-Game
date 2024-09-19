2c1d63cbd89db3adcea7c64c43b36a70faafa787
Edwin Bùi Hữu Vinh <bhvinh.brv@gmail.com>
2024-09-19 15:30:34 +0700

    more model change base on new db

--- CEG_BAL/ViewModels/Admin/CreateNewHomework.cs
+++ CEG_BAL/ViewModels/Admin/CreateNewHomework.cs
@@ -8,10 +8,6 @@ namespace CEG_BAL.ViewModels.Admin
 {
     public class CreateNewHomework
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public int TotalPoint { get; set; }
-        public int WordAmount { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string SessionTitle { get; set; }
---
+++ CEG_DAL/Models/HomeworkAnswer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CEG_DAL.Models;
+
+public partial class HomeworkAnswer
+{
+    public int HomeworkAnswerId { get; set; }
+
+    public int HomeworkQuestionId { get; set; }
+
+    public string? Answer { get; set; }
+
+    public virtual HomeworkQuestion HomeworkQuestion { get; set; } = null!;
+}
