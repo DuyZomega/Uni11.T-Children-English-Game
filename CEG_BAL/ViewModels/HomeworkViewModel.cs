e8aa8ed3f467a4102daa3e6f2f7ee08c4b4ed938
Echo2craft <justechojepar@gmail>
2024-09-23 12:19:55 +0700

    tweak html structure and edit services

--- CEG_BAL/ViewModels/HomeworkViewModel.cs
+++ CEG_BAL/ViewModels/HomeworkViewModel.cs
@@ -9,6 +9,7 @@ namespace CEG_BAL.ViewModels
 {
     public class HomeworkViewModel
     {
+        public int? HomeworkId { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
         public int? Hours { get; set; }
