55a36f58337ecc14b8666049db1b52145029dbcd
Echo2craft <justechojepar@gmail>
2024-09-23 10:28:49 +0700

    fix syncing namespace bug in pages

--- CEG_WebMVC/Models/ViewModels/Homework/Create/CreateHomeworkVM.cs
+++ CEG_WebMVC/Models/ViewModels/Homework/Create/CreateHomeworkVM.cs
@@ -5,7 +5,6 @@ namespace CEG_WebMVC.Models.ViewModels.Homework.Create
     public class CreateHomeworkVM
     {
         public int? Hours { get; set; }
-
         public string? Status { get; set; }
     }
 }
