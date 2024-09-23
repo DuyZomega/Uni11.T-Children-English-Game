e642dd91b63596d41f81867244631c3beaed5796
Edwin Bùi Hữu Vinh <bhvinh.brv@gmail.com>
2024-09-23 09:18:59 +0700

    temporal fix

--- CEG_BAL/ViewModels/Admin/CreateNewCourse.cs
+++ CEG_BAL/ViewModels/Admin/CreateNewCourse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,5 +19,6 @@ namespace CEG_BAL.ViewModels.Admin
         public int? RequiredAge { get; set; }
         public string? Difficulty { get; set; }
         public string? Category { get; set; }
+        public IFormFile? CourseImage { get; set; }
     }
 }
