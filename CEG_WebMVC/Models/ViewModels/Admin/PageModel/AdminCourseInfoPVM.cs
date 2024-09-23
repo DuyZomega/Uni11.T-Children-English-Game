a6ff71bdc532b8f6f7d6bd02068c977c4af6cda0
Echo2craft <justechojepar@gmail>
2024-09-23 10:20:26 +0700

    syncing namespace

--- CEG_WebMVC/Models/ViewModels/Admin/PageModel/AdminCourseInfoPVM.cs
+++ CEG_WebMVC/Models/ViewModels/Admin/PageModel/AdminCourseInfoPVM.cs
@@ -4,21 +4,21 @@ using CEG_WebMVC.Models.ViewModels.Course.Update;
 using CEG_WebMVC.Models.ViewModels.Session.Create;
 using CEG_WebMVC.Models.ViewModels.Session.Get;
 
-namespace CEG_WebMVC.Models.ViewModels.Course.PageModel
+namespace CEG_WebMVC.Models.ViewModels.Admin.PageModel
 {
     public class AdminCourseInfoPVM
     {
         public AdminCourseInfoPVM(
-            CourseInfoVM? courseInfo = null, 
-            UpdateCourseVM? updateCourseInfo = null, 
+            CourseInfoVM? courseInfo = null,
+            UpdateCourseVM? updateCourseInfo = null,
             List<SessionInfoVM>? sessions = null,
             CreateSessionVM? createSession = null)
         {
-            
+
             CourseInfo = courseInfo ?? new CourseInfoVM();
             UpdateCourseInfo = updateCourseInfo ?? new UpdateCourseVM();
             Sessions = sessions ?? new List<SessionInfoVM>();
-            CreateSession = createSession ?? new CreateSessionVM(CourseInfo.CourseId,CourseInfo.CourseName);
+            CreateSession = createSession ?? new CreateSessionVM(CourseInfo.CourseId, CourseInfo.CourseName);
         }
         public CourseInfoVM? CourseInfo { get; set; }
         public UpdateCourseVM? UpdateCourseInfo { get; set; }
