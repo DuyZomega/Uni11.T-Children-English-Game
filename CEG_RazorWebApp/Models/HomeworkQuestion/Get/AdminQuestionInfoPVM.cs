725471d4b49bc5f0e1280ae698a5ce3f5519594b
Echo2craft <justechojepar@gmail>
2024-10-03 09:41:57 +0700

    finished homework info page

--- CEG_RazorWebApp/Models/HomeworkQuestion/Get/AdminQuestionInfoPVM.cs
+++ CEG_RazorWebApp/Models/HomeworkQuestion/Get/AdminQuestionInfoPVM.cs
@@ -1,6 +1,28 @@
-﻿namespace CEG_RazorWebApp.Models.HomeworkQuestion.Get
+﻿using CEG_RazorWebApp.Models.HomeworkQuestion.Update;
+
+namespace CEG_RazorWebApp.Models.HomeworkQuestion.Get
 {
     public class AdminQuestionInfoPVM
     {
+        public AdminQuestionInfoPVM(
+            int? courseId,
+            int? sessionId,
+            int? homeworkId,
+            QuestionInfoVM? question = null,
+            UpdateQuestionVM? updateQuestionInfo = null
+            )
+        {
+            CourseId = courseId;
+            SessionId = sessionId;
+            HomeworkId = homeworkId;
+            QuestionInfo = question ?? new QuestionInfoVM();
+            UpdateQuestionInfo = updateQuestionInfo ?? new UpdateQuestionVM();
+        }
+        public int? CourseId { get; set; }
+        public int? SessionId { get; set; }
+        public int? HomeworkId { get; set; }
+        public string? HomeworkStatus { get; set; }
+        public QuestionInfoVM? QuestionInfo { get; set; }
+        public UpdateQuestionVM? UpdateQuestionInfo { get; set; }
     }
 }
