dd9c5e0e0bbae456b63059072c393f916cf4f939
khanhlinh00 <linhnkse140459@fpt.edu.vn>
2024-10-07 10:57:21 +0700

    added StudentInfo

---
+++ CEG_RazorWebApp/Models/Student/Get/StudentInfoVM.cs
@@ -0,0 +1,29 @@
+﻿using CEG_DAL.Models;
+
+namespace CEG_RazorWebApp.Models.Student.Get
+{
+    public class StudentInfoVM
+    {
+        public int StudentId { get; set; }
+
+        public int ParentId { get; set; }
+
+        public int AccountId { get; set; }
+
+        public int Highscore { get; set; }
+
+        public string Description { get; set; } = null!;
+
+        public int? Playtime { get; set; }
+
+        public int? CurLevel { get; set; }
+
+        public int? Points { get; set; }
+
+        public int? Age { get; set; }
+
+        public DateTime? Birthdate { get; set; }
+
+        public string? Image { get; set; }
+    }
+}
